c45fb649b32413905cff76d6702d7c372041af64
Nenad Mikša <nenad.miksa@microblink.com>
2026-02-07 17:32:57 +0100

    ignore conversion warnings in gtest's headers

--- main/tests/hello-test-file.cc
+++ main/tests/hello-test-file.cc
@@ -1,6 +1,9 @@
 #include <Paths.h>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcharacter-conversion"
 #include <gtest/gtest.h>
+#pragma clang diagnostic pop
 
 TEST(HelloGreetTest, ReadFile)
 {
