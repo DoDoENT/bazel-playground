5eb9169ff9795c1cde1d426c1725fc40937c4317
Nenad Mikša <nenad.miksa@microblink.com>
2026-05-23 18:09:54 +0200

    fix build on newer xcode

--- main/tests/hello-test-default.cc
+++ main/tests/hello-test-default.cc
@@ -1,9 +1,7 @@
 #include "main/hello-greet.h"
 
 #pragma clang diagnostic push
-#ifndef __APPLE__
 #pragma clang diagnostic ignored "-Wcharacter-conversion"
-#endif
 #include <gtest/gtest.h>
 #pragma clang diagnostic pop
 
