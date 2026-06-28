d3bf0e7af4501f23acebc4fd4f74e32bba8bd0b0
DoDo <DoDoENT@users.noreply.github.com>
2026-06-28 12:40:57 +0200

    Feature/opencv test (#63)
    * add simple test that uses opencv
    
    * ensure LFS data is checked-out and bump Android API to 24
    
    * fix image loading on iOS
    
    * fix building opencv binaries
    
    * fix loading of test image on android
    
    * ignore warnings for external libs
    
    * disable OpenCV test for WASM basic as OpenCV 5 now requires SIMD support
    
    * disable "external_include_paths" feature as it's not yet compatible with hermetic LLVM

---
+++ opencv/image-manipulation.cc
@@ -0,0 +1,59 @@
+#include <Paths.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcharacter-conversion"
+#include <gtest/gtest.h>
+#pragma clang diagnostic pop
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wconversion"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#pragma clang diagnostic pop
+
+
+#ifdef __ANDROID__
+#include "PathProvider.hpp"
+#endif
+
+namespace 
+{
+    std::string resolveImagePath( std::string_view filename )
+    {
+#ifdef __ANDROID__
+        return AndroidPaths::internalStoragePath() + '/' + resolveTestDataPath( filename );
+#else 
+        return resolveTestDataPath( filename );
+#endif
+    }
+}
+
+TEST(OpenCVTest, resizeAndGrayscale)
+{
+    auto path{ resolveImagePath( "image.png" ) };
+    std::cout << "Loading file: " << path << std::endl;
+    cv::Mat inputImage{ cv::imread( path, cv::IMREAD_COLOR ) };
+
+    ASSERT_FALSE( inputImage.empty() ) << "Failed to load file: " << path;
+
+    std::cout << "Input image size: " << inputImage.cols << "x" << inputImage.rows << std::endl;
+
+    cv::Mat grayImage;
+    cv::cvtColor( inputImage, grayImage, cv::COLOR_BGR2GRAY );
+
+    cv::Mat resizedImage;
+    cv::resize( grayImage, resizedImage, cv::Size{ 103, 120 } );
+
+    auto const expectedPath{ resolveImagePath( "expected-result.png" ) };
+
+    cv::Mat expectedImage{ cv::imread( expectedPath, cv::IMREAD_GRAYSCALE ) };
+
+    cv::Mat diffImg;
+    cv::absdiff( resizedImage, expectedImage, diffImg );
+
+    auto const pixelSum{ cv::sum( diffImg ) };
+
+    EXPECT_EQ( 0, pixelSum.val[ 0 ] );
+}
